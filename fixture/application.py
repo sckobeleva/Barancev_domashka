723fd096dba181b982296285b61694b5509337bf
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-06-18 22:24:55 +0400

    Create aided classes

---
+++ fixture/application.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
+class Application:
+
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(60)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+
+    def open_home_page(self):  # открываем главную страницу
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
+
+    def destroy(self):
+        self.driver.quit()
+
+
+
+
+
+
+
+
+
---
+++ fixture/group.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+
+
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def open_groups_page(self):  # переходим на страницу "groups"
+        driver = self.app.driver
+        driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def create(self, group):  # создаем новую группу, заполняем поля, сохраняем
+        driver = self.app.driver
+        self.open_groups_page()
+        driver.find_element(By.CSS_SELECTOR, "[name = ""new""]").click()
+        driver.find_element(By.NAME, "group_name").click()
+        driver.find_element(By.NAME, "group_name").send_keys(group.name)
+        driver.find_element(By.NAME, "group_header").click()
+        driver.find_element(By.NAME, "group_header").send_keys(group.header)
+        driver.find_element(By.NAME, "group_footer").click()
+        driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        driver.find_element(By.NAME, "submit").click()
+        self.return_to_group_page()
+
+    def return_to_group_page(self):  # возвращаемся на страницу "groups"
+        driver = self.app.driver
+        driver.find_element(By.LINK_TEXT, "group page").click()
