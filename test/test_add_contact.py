9240f134e46c12e70635f590ab3b738f58ff5b64
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-06-24 15:30:11 +0400

    Replace login/logout to conftest.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,6 +2,4 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="aaa", middlename="aaa", lastname="aaa", nickname="aaa"))
-    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,12 +2,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="444", header="444", footer="444"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
