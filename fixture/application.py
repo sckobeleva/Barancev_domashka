18ae94b5572b722e1b3c5f74f0e38646c2cd0f25
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-06-24 19:12:42 +0400

    Change waiting

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,7 @@ class Application:
 
     def __init__(self):
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(60)
+        self.driver.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
