3fd547baace953f9db0be5e104070868074ad76a
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-06-27 18:20:58 +0400

    Add comparing of group lists

--- model/group.py
+++ model/group.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Group:
 
     def __init__(self, name=None, header=None, footer=None, id=None):
@@ -10,4 +13,10 @@ class Group:
         return "%s:%s" % (self.id, self.name)
 
     def __eq__(self, other):    # сравниваем группы по смыслу, а не по физ.расположению, чтобы сравнение не падало
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(self):  # получаем ключ, по которому будем сравнивать группы
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
