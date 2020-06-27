1e98399da772ad98268198dd4cb5bd5aa127c3c5
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-06-27 17:03:32 +0400

    Add check that lists before/after are same

--- model/group.py
+++ model/group.py
@@ -5,3 +5,9 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    def __repr__(self): # получаем строковое представление объекта
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):    # сравниваем группы по смыслу, а не по физ.расположению, чтобы сравнение не падало
+        return self.id == other.id and self.name == other.name
