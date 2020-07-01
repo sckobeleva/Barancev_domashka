2001417c381115a413b1082849881ac08bab7337
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-07-01 12:14:01 +0400

    Add aided methods for contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from model.contact import Contact
 
 
 class ContactHelper:
@@ -6,19 +7,56 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
+    def add_new_contact(self):  # переходим на страницу "add new" для создания контакта
+        driver = self.app.driver
+        if not (driver.current_url.endswith("/edit.php") and len(driver.find_elements_by_name("submit")) > 0):
+            driver.find_element(By.LINK_TEXT, "add new").click()
+
+    def count(self):    # получаем количество контактов в списке
+        driver = self.app.driver
+        self.app.open_home_page()
+        return len(driver.find_elements(By.CSS_SELECTOR, "td input"))
+
     def create(self, contact):  # заполняем поля в карточке, сохраняем
         driver = self.app.driver
-        self.open_contacts_page()
+        self.add_new_contact()
         driver.find_element(By.NAME, "firstname").click()
         driver.find_element(By.NAME, "firstname").send_keys(contact.firstname)
-        driver.find_element(By.NAME, "middlename").click()
-        driver.find_element(By.NAME, "middlename").send_keys(contact.middlename)
         driver.find_element(By.NAME, "lastname").click()
         driver.find_element(By.NAME, "lastname").send_keys(contact.lastname)
-        driver.find_element(By.NAME, "nickname").click()
-        driver.find_element(By.NAME, "nickname").send_keys(contact.nickname)
+        driver.find_element(By.NAME, "home").click()
+        driver.find_element(By.NAME, "home").send_keys(contact.homephone)
+        driver.find_element(By.NAME, "mobile").click()
+        driver.find_element(By.NAME, "mobile").send_keys(contact.mobilephone)
+        driver.find_element(By.NAME, "work").click()
+        driver.find_element(By.NAME, "work").send_keys(contact.workphone)
+        driver.find_element(By.NAME, "fax").click()
+        driver.find_element(By.NAME, "fax").send_keys(contact.faxphone)
         driver.find_element(By.NAME, "submit").click()
+        self.app.open_home_page()
+        self.contact_cache = None
 
-    def open_contacts_page(self):  # переходим на страницу "add new"
-        driver = self.app.driver
-        driver.find_element(By.LINK_TEXT, "add new").click()
+    contact_cache = None
+
+    def get_contact_list(self):   # получаем список контактов
+        if self.contact_cache is None:
+            driver = self.app.driver
+            self.app.open_home_page()
+            self.contact_cache = []
+            for elem in driver.find_elements_by_name("entry"):
+                id = elem.find_element_by_name("selected[]").get_attribute('id')
+                lastname = elem.find_elements_by_css_selector("td")[1].text
+                firstname = elem.find_elements_by_css_selector("td")[2].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=str(id)))
+        return list(self.contact_cache)
+
+    #def open_contact_to_edit_by_index(self, index): # находим и открываем контакт на редактирование по индексу
+
+
+    #def open_contact_to_view_by_index(self, index): # находим и открываем контакт на просмотр по индексу
+
+
+
+
+
+
--- model/contact.py
+++ model/contact.py
@@ -1,7 +1,26 @@
+from sys import maxsize
+
+
 class Contact:
 
-    def __init__(self, firstname, middlename, lastname, nickname):
+    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None, faxphone=None, id=None):
         self.firstname = firstname
-        self.middlename = middlename
         self.lastname = lastname
-        self.nickname = nickname
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.faxphone = faxphone
+        self.id = id
+
+    def __repr__(self):  # получаем строковое представление объекта
+        return "%s:%s %s" % (self.id, self.firstname, self.lastname)
+
+    def __eq__(self, other):  # сравниваем по смыслу, а не по физ.расположению, чтобы сравнение не падало
+        return (self.id is None or other.id is None or self.id == other.id) \
+               and self.firstname == other.firstname and self.lastname == other.lastname
+
+    def id_or_max(self):  # получаем ключ, по которому будем сравнивать
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
