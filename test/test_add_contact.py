4c1c4ca337f824e277a17d4ca7c85da1a5cd5637
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-07-01 12:12:30 +0400

    Edit test test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,4 +2,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.contact.create(Contact(firstname="aaa", middlename="aaa", lastname="aaa", nickname="aaa"))
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="aaa", lastname="bbb", homephone="001", mobilephone="002", workphone="003", faxphone="004")
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
