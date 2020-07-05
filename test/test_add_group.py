019b66e597747393810c3ec56a8e4eb1ccffea97
sckobeleva <64254934+sckobeleva@users.noreply.github.com>
2020-07-05 14:13:26 +0400

    Add parameterization and generate random testdata

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,33 @@
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+# в список символов можно еще добавить + string.punctuation
+
+# полный перебор всех возможных случаев: 3 поля, 2 значения (пустое и рандомное) - запустится 8 тестов
+#testdata = [
+#    Group(name=name, header=header, footer=footer)
+#    for name in ["", random_string("name", 10)]
+#    for header in ["", random_string("header", 20)]
+#    for footer in ["", random_string("footer", 20)]
+#]
+
+# второй вариант рандомных данных
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])    # 3 параметр выводит в отчете питест тестовые данные, подготовленные функцией __repr__
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="444", header="444", footer="444")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-#def test_add_empty_group(app):
-#    old_groups = app.group.get_group_list()
-#    group = Group(name="", header="", footer="")
-#    app.group.create(group)
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) + 1 == len(new_groups)
-#    old_groups.append(group)
-#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
